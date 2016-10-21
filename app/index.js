3f16c9d74c802107210b18870ab062d1cb553855
Derrick Bray <derrickbray@yahoo.com>
2016-10-20 20:38:29 -0500

    model

--- app/index.js
+++ app/index.js
@@ -4,10 +4,17 @@ import NetflixResultView from 'views/netflix-result.js';
 // Create a new instance of the view passing in the result element and an empty object as the arguments
 //
 
-const searchForm = ;
-const searchInput = ;
-const searchResult = ;
+const searchForm =;
+const searchInput =;
+const searchResult =;
 
-NetflixResultView(searchResult, []) {
+NetflixResultView(searchResult, []) {};
 
-};
+const uv = new NetflixResultView(document.querySelector('.movie'), {});
+
+fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan').then((res) => res.json()).then((data) => {
+    const movie = data.results[0];
+
+    uv.model = movie;
+    uv.render();
+});
--- app/views/netflix-result.js
+++ app/views/netflix-result.js
@@ -15,7 +15,6 @@ export default class NetflixResultView {
   }
 }
 
-
 // {
 //   "unit": 883,
 //   "show_id": 70299043,

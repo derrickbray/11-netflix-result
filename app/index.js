dc8c8354fb6b257c325b8a7206f9b7dd949903f4
Derrick Bray <derrickbray@yahoo.com>
2016-10-21 14:43:55 -0500

    js is attached

--- app/index.js
+++ app/index.js
@@ -1,26 +1,19 @@
 import 'whatwg-fetch';
-import NetflixResultView from './views/netflix-result.js';
+import NetflixResultView from './views/netflix-result';
 // Create Variables for the search form, search input, and search result
 // Create a new instance of the view passing in the result element and an empty object as the arguments
 //
 
-// const searchForm();
-// const searchResult{};
-console.log('hello');
+const movieEl = document.querySelector('.movie');
+const input = document.querySelector('.search__title');
+const searchView = new NetflixResultView(movieEl, {});
 
-const searchInput = NetflixResultView(searchResult, []) {};
-
-searchInput.then((info) => {
-  after.innerText = info;
-})
-
-const nf = new NetflixResultView(document.querySelector('.movie'), {});
 
 fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
-.then((searchResult) => searchResult.json())
-.then((data) => {
-    const movie = data.results[0];
-    nf.model = movie;
-    nf.render();
-});
+  .then((searchResult) => searchResult.json())
+  .then((data) => {
+    const movie = data;
+    searchView.model = movie;
+    searchView.render();
+  });
 // debugger;

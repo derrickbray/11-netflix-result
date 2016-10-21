6f7fda60e7855790ddb6f5f3e58a91d67297f290
Derrick Bray <derrickbray@yahoo.com>
2016-10-21 08:36:05 -0500

    trying to fetch

--- app/index.js
+++ app/index.js
@@ -5,16 +5,33 @@ import NetflixResultView from 'views/netflix-result.js';
 //
 
 const searchForm =;
-const searchInput =;
 const searchResult =;
 
-NetflixResultView(searchResult, []) {};
+const searchInput = NetflixResultView(searchResult, []) {};
+
+searchInput.then((info) => {
+  after.innerText = info;
+})
+// debugger;
 
 const uv = new NetflixResultView(document.querySelector('.movie'), {});
 
-fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan').then((res) => res.json()).then((data) => {
+fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+.then((result) => result.json())
+.then((data) => {
     const movie = data.results[0];
-
     uv.model = movie;
     uv.render();
 });
+
+// const uv = new UserView(document.querySelector('.user'), {});
+//
+// fetch('https://randomuser.me/api')
+//   .then((result) => result.json())
+//   .then((data) => {
+//     const person = data.results[0];
+//     console.log(person);
+//     uv.model = person;
+//     uv.render();
+//     document.querySelector('.user__first-name').innerText = person.name.first;
+//   });
--- app/views/netflix-result.js
+++ app/views/netflix-result.js
@@ -7,9 +7,9 @@ export default class NetflixResultView {
 
   render() {
     this.el.querySelector('.movie__show-title').innerText = this.model.show_title;
-    this.el.querySelector('.movie__category').innerText = this.model.category;
-    this.el.querySelector('.movie__rating').innerText = this.model.rating;
-    this.el.querySelector('.movie__picture').src = this.model.poster;
+    this.el.querySelector('.movie__show-category').innerText = this.model.category;
+    this.el.querySelector('.movie__show-rating').innerText = this.model.rating;
+    this.el.querySelector('.movie__show-picture').src = this.model.poster;
     this.el.querySelector('.movie__summary').innerText = this.model.summary;
     this.el.querySelector('.movie__show-cast').innerText = this.model.show_cast;
   }

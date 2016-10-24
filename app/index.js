ba8d62a35b819f0b70b26bf15642edb724703b15
Derrick Bray <derrickbray@yahoo.com>
2016-10-24 09:02:23 -0500

    event listener

--- app/index.js
+++ app/index.js
@@ -7,7 +7,16 @@ import NetflixResultView from './views/netflix-result';
 const movieEl = document.querySelector('.movie');
 const input = document.querySelector('.search__title');
 const searchView = new NetflixResultView(movieEl, {});
+const form = document.querySelector('.search__button-btn');
+// const response = document.querySelector('.response');
 
+form.addEventListener('submit', (ev) => {
+  ev.preventDefault();
+
+  input.innerText = `
+  thank you for your request but we can't find: "${input.value}".
+  `;
+});
 
 fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
   .then((searchResult) => searchResult.json())

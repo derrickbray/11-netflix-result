10c2af1ed27be7002446a4998a237965fa628b8c
Derrick Bray <derrickbray@yahoo.com>
2016-11-18 20:05:39 -0600

    fixed the search bar

--- app/index.js
+++ app/index.js
@@ -3,22 +3,26 @@ import NetflixResultView from './views/netflix-result';
 // Create Variables for the search form, search input, and search result
 // Create a new instance of the view passing in the result element and an empty object as the arguments
 //
-
+var apiUrl = 'http://netflixroulette.net/api/api.php';
 const movieEl = document.querySelector('.movie');
 const input = document.querySelector('.search__title');
 const searchView = new NetflixResultView(movieEl, {});
-const form = document.querySelector('.search__button-btn');
+const form = document.querySelector('.search');
 // const response = document.querySelector('.response');
 
 form.addEventListener('submit', (ev) => {
   ev.preventDefault();
 
-  input.innerText = `
-  thank you for your request but we can't find: "${input.value}".
-  `;
+  fetch(`${apiUrl}?title=${input.value}`)
+    .then((searchResult) => searchResult.json())
+    .then((data) => {
+      const movie = data;
+      searchView.model = movie;
+      searchView.render();
+    });
 });
 
-fetch('http://netflixroulette.net/api/api.php?title=Attack%20on%20titan')
+fetch(`${apiUrl}?title=Attack%20on%20titan`)
   .then((searchResult) => searchResult.json())
   .then((data) => {
     const movie = data;
--- app/views/netflix-result.js
+++ app/views/netflix-result.js
@@ -14,18 +14,3 @@ export default class NetflixResultView {
     this.el.querySelector('.movie__show-cast').innerText = this.model.show_cast;
   }
 }
-
-// {
-//   "unit": 883,
-//   "show_id": 70299043,
-//   "show_title": "Attack on Titan",
-//   "release_year": "2013",
-//   "rating": "4.6",
-//   "category": "Anime",
-//   "show_cast": "Yuki Kaji, Yui Ishikawa, Marina Inoue, Daisuke Ono, Hiro Shimono, Hiroshi Kamiya, Keiji Fujiwara, Kishô Taniyama, Romi Park, Ryota Ohsaka",
-//   "director": "",
-//   "summary": "For over a century, people have been living behind barricades to block out the giant Titans that threaten to destroy the human race. When a Titan destroys his hometown, young Eren Yeager becomes determined to fight back.",
-//   "poster": "http://netflixroulette.net/api/posters/70299043.jpg",
-//   "mediatype": 1,
-//   "runtime": "24 min"
-// }
